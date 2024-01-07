7f2d5fd66e65b9f693f6bd0ddfc35f1c0004d44b
Sadullo <sadulloburiyev1992@gmail.com>
2024-01-07 17:48:49 +0500

    feat: solve hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,33 @@
+import fs from "fs/promises";
+import { existsSync } from "fs";
+
+const { createHash } = await import("crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readFilePath = "./src/hash/files/fileToCalculateHashFor.txt";
+
+  const hash = createHash("sha256");
+  
+  hash.on("readable", () => {
+    const data = hash.read();
+    if (data) {
+      console.log(data.toString("hex"));
+    }
+  });
+
+  try {
+    if (!existsSync(readFilePath)) throw new Error("FS operation failed");
+
+    const content = await fs.readFile(readFilePath, {
+      encoding: "utf8",
+    });
+
+    hash.write(content);
+  } catch (err) {
+    console.log(err);
+  }  
+
+  hash.end();
 };
 
 await calculateHash();
