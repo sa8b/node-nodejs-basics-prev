9fc2ddf913afbb8609e9573b6362786bf8adff9a
Sadullo <sadulloburiyev1992@gmail.com>
2024-01-09 21:07:54 +0500

    feat: add zip tasks solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { createWriteStream, createReadStream, unlink } from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    const readFilePath = "./src/zip/files/fileToCompress.txt"; 
+    const writeFilePath = "./src/zip/files/archive.gz"; 
+
+  const gzip = createGzip();
+  const source = createReadStream(readFilePath);
+  const destination = createWriteStream(writeFilePath);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    } else {
+      unlink(readFilePath, (err) => {
+        if(err) console.error(err);
+      });
+    }
+  });
 };
 
 await compress();
