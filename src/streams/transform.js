6894da66b05360e45b65e24857b2ae5c02f9b732
Sadullo <sadulloburiyev1992@gmail.com>
2024-01-09 17:43:00 +0500

    feat: add solutions to streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const readable = process.stdin;
+    const writable = process.stdout;
+
+    const transform = new Transform({
+        transform(chunk, enc, cb) {
+            const chunkStringified = chunk.toString().trim();
+
+            const revesedChunk = chunkStringified.split('').reverse().join('');
+
+            this.push(revesedChunk + '\n');
+
+            cb();
+        }
+    });
+
+    pipeline(readable, transform, writable, (err) => {
+        console.error(`Error: ${err}`);
+    })
 };
 
 await transform();
